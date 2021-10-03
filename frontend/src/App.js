f10afdac7f611ae4e9d75254f68b48c9261b11d1
Raghav <codingkapur@gmail.com>
2021-10-03 10:16:05 +0530

    create nav links component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,10 +8,12 @@ import {
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 function App() {
   return (
     <Router>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact={true}>
           <Users />
---
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,23 @@
+import "./NavLinks.css";
+import { NavLink } from "react-router-dom";
+
+const NavLinks = () => {
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/" exact>All Users</NavLink>
+      </li>
+      <li>
+        <NavLink to="/u1/places">My Places</NavLink>
+      </li>
+      <li>
+        <NavLink to="/places/new">New place</NavLink>
+      </li>
+      <li>
+        <NavLink to="/auth">Authenticate</NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default NavLinks;
