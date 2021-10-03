792dff64ea556267e20bafce2a552d902a2d94c8
Raghav <codingkapur@gmail.com>
2021-10-03 10:57:23 +0530

    install CSSTransition library and implement side drawer in mobile view

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,23 +1,45 @@
-import "./MainNavigation.css";
-import { Link } from 'react-router-dom';
-import MainHeader from "./MainHeader";
-import NavLinks from './NavLinks';
+import React from "react";
+import {useState} from 'react'
 
+import "./MainNavigation.css";
+import { Link, NavLink } from "react-router-dom";
+import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import Backdrop from '../UIElements/Backdrop'
 const MainNavigation = () => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    }
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    }
+
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
+    <React.Fragment>
+        {drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
+        <SideDrawer show={drawerIsOpen} onClick={closeDrawer}>
+        <nav className="main-navigation__drawer-nav">
+        <NavLinks />
+        </nav>
+      </SideDrawer>
+      
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
           <Link to="/">Your Places</Link>
-      </h1>
-      <nav>
-          <NavLinks /> 
-      </nav>
-    </MainHeader>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
